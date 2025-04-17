dfbd1ac8da1fc729dbadbe286dfb27e82c9f8e08
sajida246 <136786181+sajida246@users.noreply.github.com>
2025-04-17 16:06:21 +0530

    Update Vapi.js

--- src/components/Vapi.js
+++ src/components/Vapi.js
@@ -3,7 +3,7 @@ import Vapi from "@vapi-ai/web";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-const vapi = new Vapi("4375ab95-9cc1-4d70-8377-c69c9fcebaf6");
+const vapi = new Vapi("5b1f521d-3fe6-40f9-8f6f-19e6c2d21bad");
 
 export default function VapiAssistant() {
   const [isTalking, setIsTalking] = useState(false);
@@ -14,7 +14,7 @@ export default function VapiAssistant() {
         
         if(!isTalking){
           console.log("starting")
-          await vapi.start("0776373c-104e-4d7d-abc5-c7c37a713a8e");
+          await vapi.start("62b8ae12-d6df-40de-af36-d01e54d94e1c");
           setIsTalking(true);
         }
         else {
